7f4a1b36d706df4e682a28c2539fef4ad40ae9eb
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-13 21:24:23 +0200

    task

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,4 @@
 export default function getListStudents() {
-
   const objArr = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }];
