da9a39ed1c256d3c55b5e195e14f54bff0b9eb63
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-02-14 22:22:23 +0200

    task

---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,17 @@
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingReact?: number
+    }
+
+    class React extends Subjects.Subject {
+        getRequirements(): string {
+            return 'Here is the list of requirements for React';
+        }
+
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingReact === 0)
+                return "No available teacher";
+            return `Available Teacher: ${this.teacher.firstName}`;
+        }
+    }
+}
