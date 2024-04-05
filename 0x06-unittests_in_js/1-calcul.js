fc30b54bc014f95c23b3fefed855a686066261cc
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-04-04 18:06:23 -0700

    task

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,15 @@
+function calculateNumber(a, b, type) {
+	const arounded = Math.round(a);
+	const brounded = Math.round(b);
+	if (type === 'SUM')
+		return c = arounded + brounded;
+	if (type === 'SUBTRACT')
+		return c = arounded - brounded;
+	if (type === 'DIVIDE') {
+		if (brounded === 0)
+			return 'Error';
+		return arounded / brounded;
+	}
+}
+
+module.exports = calculateNumber;
