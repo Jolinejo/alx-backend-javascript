2361c8b0623c02a3b7c4bc05fa2701b84989f0f1
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-04-05 18:29:31 +0200

    task

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
 // 1-stdin.js
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.on('readable', (data) => {
+  const name = process.stdin.read();
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
